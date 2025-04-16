d462600572f59d9008b0a3a733faefd53a080873
Maylon Henrique de Oliveira <moliveira@servidor.acad>
2025-04-16 11:14:31 -0300

    aula 07

---
+++ aula07/dados.php
@@ -0,0 +1,8 @@
+<h1>Dados recebidos do formulário</h1>
+
+<?php
+echo "Nome: " . $_GET['nome'];
+echo "<br/>";
+echo "Idade: " . $_GET['idade'];
+
+?>
---
+++ aula07/index.php
@@ -0,0 +1,10 @@
+<h1>Introdução aos formulários e envio de dados.</h1>
+<form method="GET" action="dados.php">
+<label>Nome:</label>
+<input type="text" name="nome">
+<br/><br/>
+<label>Idade:</label>
+<input type="text" name="idade">
+<br/><br/>
+<button>Enviar</button>
+</form>
