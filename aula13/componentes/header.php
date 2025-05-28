70267c0a353fc0412c65d199a738375ac9ab4971
Maylon Henrique de Oliveira <moliveira@servidor.acad>
2025-05-28 11:57:03 -0300

    aula13 - login

---
+++ aula13/componentes/header.php
@@ -0,0 +1,17 @@
+<?php 
+include '../php/config.php'; 
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lula Cosméticos</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<!-- MENU INICIO -->
+    <?php include 'navbar.php'; ?>
+<!-- MENU FIM -->
