c79c2e33e3f30a09737749d2f3270b4c26aca4cd
Maylon Henrique de Oliveira <maylon.oliveira@e239.local>
2025-10-29 14:25:10 -0300

    updated view and included persistent data

--- projeto_t1/aula15/views/categoria_terror.php
+++ projeto_t1/aula15/views/categoria_terror.php
@@ -1,6 +1,6 @@
 <!-- Cabeçalho do Site -->
-<?php 
-include './componentes/header.php'; 
+<?php
+include './componentes/header.php';
 require '../conexao.php';
 ?>
 
@@ -9,10 +9,32 @@ require '../conexao.php';
 <!-- Conteúdo do Site -->
 <div class="container">
     <h1>Resenhas de Terror</h1>
-    <h3>Aqui você encontra as resenhas mais tenebrosas do 2º ano do MTec do Centro Paula Souza</h3>
+    <h4>Aqui você encontra as resenhas mais tenebrosas do 2º ano do MTec do Centro Paula Souza</h4>
+
+    <div class="row">
+        <?php
+        $sql = "SELECT * FROM obras WHERE categoria='terror'";
+        $resultado = mysqli_query($conexao, $sql);
+        ?>
+        <?php
+        while ($linha = mysqli_fetch_assoc($resultado)) {
+            echo '
+                <div class="card m-2" style="width: 18rem;">
+                    <img src="'.BASE_URL.'public/imagens/obras/'.$linha["img"].'" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"> ' . $linha["nome"] . ' </h5>
+                        <p class="card-text"> ' . mb_substr($linha['sinopse'],0 , 100)  . ' ...</p>
+                        <a href="#" class="btn btn-primary">Ver Mais</a>
+                    </div>
+                </div>
+                ';
+        }
+        ?>
 
 
-    
+    </div>
+
+
 </div>
 <!-- Final do Conteúdo do Site -->
 
